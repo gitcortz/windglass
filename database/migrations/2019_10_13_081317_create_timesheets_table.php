b85058f563fdf1b14c624cbf3cd5281f3bbddf87
mcortes <mykmyk@gmail.com>
2019-10-13 16:17:41 +0800

    add Timesheet table

---
+++ database/migrations/2019_10_13_081317_create_timesheets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimesheetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timesheets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('time_in');
+            $table->string('time_out');
+            $table->timestamps();
+
+            $table->biginteger('employee_id')->nullable()->unsigned();  
+            $table->foreign('employee_id')->references('id')->on('employees'); 
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timesheets');
+    }
+}
